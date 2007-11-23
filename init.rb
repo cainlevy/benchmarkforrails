88fa7a96d5012b87a6398203b76f10f372c1020d
Lance Ivy <lance@cainlevy.net>
2007-11-23 21:54:10 +0000

    initial project import
    git-svn-id: http://benchmarkforrails.googlecode.com/svn/trunk@2 24d200a0-9f3f-0410-8e87-2924e6406934

---
+++ init.rb
@@ -0,0 +1,21 @@
+if RAILS_ENV == 'development'
+
+BenchmarkForRails.watch("request processing", ActionController::Base, :process)
+BenchmarkForRails.watch("respond_to block", ActionController::MimeResponds::InstanceMethods, :respond_to)
+BenchmarkForRails.watch("activerecord find", ActiveRecord::Base, :find, false)
+BenchmarkForRails.watch("before filters", ActionController::Filters::InstanceMethods, :run_before_filters)
+BenchmarkForRails.watch("after filters", ActionController::Filters::InstanceMethods, :run_after_filters)
+BenchmarkForRails.watch("session startup", CGI::Session, :initialize)
+
+class ActionController::Base
+  # print reports at the end
+  def process_with_benchmark_for_rails(*args, &block) #:nodoc:
+    returning process_without_benchmark_for_rails(*args, &block) do
+      BenchmarkForRails.report
+      logger.flush if logger.respond_to? :flush
+    end
+  end
+  alias_method_chain :process, :benchmark_for_rails
+end
+
+end
