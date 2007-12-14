e747f4e47b47bea58e00190fefec9d6b397bb3c6
Lance Ivy <lance@cainlevy.net>
2007-12-14 02:32:52 +0000

    shortening the names for the built-in benchmarks
    git-svn-id: http://benchmarkforrails.googlecode.com/svn/trunk@14 24d200a0-9f3f-0410-8e87-2924e6406934

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ BenchmarkForRails.watch(:request, ::Dispatcher, :dispatch)
 
 # Processing the action itself (calling the controller method)
 # This is what Rails' default benchmarks claim is the response time.
-BenchmarkForRails.watch("processing action", ActionController::Base, :perform_action)
+BenchmarkForRails.watch("action", ActionController::Base, :perform_action)
 
 # Tries to give some perspective on how much time is tied up in reloading the
 # application for development mode.
@@ -15,8 +15,8 @@ end
 
 # Session management is normally small, although sometimes it's still a
 # significant percentage.
-BenchmarkForRails.watch("session management", CGI::Session, :initialize)
-BenchmarkForRails.watch("session management", ActionController::Base, :close_session)
+BenchmarkForRails.watch("session", CGI::Session, :initialize)
+BenchmarkForRails.watch("session", ActionController::Base, :close_session)
 
 # Controller filters
 BenchmarkForRails.watch("filters", ActionController::Filters::InstanceMethods, :run_before_filters)
@@ -26,7 +26,7 @@ BenchmarkForRails.watch("filters", ActionController::Filters::InstanceMethods, :
 # Hence why we try and watch the core finder. More watches might be added
 # to this group to form a more complete picture of database access. The
 # question is simply which methods bypass find().
-BenchmarkForRails.watch("activerecord find", ActiveRecord::Base, :find, false)
+BenchmarkForRails.watch("finders", ActiveRecord::Base, :find, false)
 
 # And yes, it's still important to know how much time is spent rendering.
 BenchmarkForRails.watch("rendering", ActionController::Base, :render)
